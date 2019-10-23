5018262bbdee66014c12beae770d83a0db142fab
qH0sT <asuslaptop230@gmail.com>
2019-10-23 17:14:54 +0300

    Audio Streaming with NAudio

---
+++ MicReciever/MicReciever/Form1.cs
@@ -0,0 +1,68 @@
+﻿using NAudio.Wave;
+using System;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace MicSendUDP
+{
+    public partial class Form1 : Form
+    {
+        Recorder rc;
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        class Recorder
+        {
+            UdpClient client = new UdpClient();
+            WaveInEvent wave = new WaveInEvent();
+            public Recorder()
+            {
+                //Thread play = new Thread(new ThreadStart(Play));
+                //play.Start();
+                wave.BufferMilliseconds = 30;
+                wave.DeviceNumber = 0;
+                wave.WaveFormat = new WaveFormat(44100, 16, 2);
+                wave.DataAvailable += Wave_DataAvailable;
+                wave.StartRecording();
+            }
+            public void Stop()
+            {
+                wave.StopRecording();
+                wave.Dispose();
+            }
+            /*
+            private void Play()
+            {
+                WaveOutEvent output = new WaveOutEvent();
+                BufferedWaveProvider buffer = new BufferedWaveProvider(new WaveFormat(8000, 16, 2));
+                output.Init(buffer);
+                output.Play();
+                for (;;)
+                {
+                    IPEndPoint remoteEP = null;
+                    byte[] data = client.Receive(ref remoteEP);
+                    buffer.AddSamples(data, 0, data.Length);
+                }
+            }
+            */
+          
+            private void Wave_DataAvailable(object sender, WaveInEventArgs e)
+            {
+                client.Send(e.Buffer, e.BytesRecorded, "127.0.0.1", 9999);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             rc = new Recorder();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rc.Stop();
+        }
+    }
+}
---
+++ MicSend/MicSend/Form1.cs
@@ -0,0 +1,68 @@
+﻿using NAudio.Wave;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MicRecieveUDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Recorder rc;
+        static  Thread play;
+        static IPEndPoint ipep;
+        static UdpClient newsock;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ipep == null && newsock == null)
+            {
+                ipep = new IPEndPoint(IPAddress.Any, 9999);
+                newsock = new UdpClient(ipep);
+                
+            }
+            rc = new Recorder();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+        class Recorder
+        {
+            public Recorder()
+            {
+                play  = new Thread(new ThreadStart(Play));
+                play.Start();
+              
+            }
+            private void Play()
+            {
+                WaveOutEvent output = new WaveOutEvent();
+                BufferedWaveProvider buffer = new BufferedWaveProvider(new WaveFormat(44100, 16, 2));
+                output.Init(buffer);
+                output.Play();
+                for (;;)
+                {
+                    IPEndPoint remoteEP = null;
+                    byte[] data = newsock.Receive(ref remoteEP);
+                    buffer.AddSamples(data, 0, data.Length);
+                }
+            }
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            play.Abort();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            play.Abort();
+        }
+    }
+}
